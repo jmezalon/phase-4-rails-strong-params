76d66608c50c65d97ba8cf9d48135d8d06cacad3
Max Mezalon <jmezalon@gmail.com>
2021-12-13 10:39:13 -0500

    commit tree bd47817417cb470363b52a710c87871ffe0e3079

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,5 +1,5 @@
 class BirdsController < ApplicationController
-
+  wrap_parameters format: []
   # GET /birds
   def index
     birds = Bird.all
@@ -8,10 +8,10 @@ class BirdsController < ApplicationController
 
   # POST /birds
   def create
-    bird = Bird.create(name: params[:name], species: params[:species])
+    bird = Bird.create(bird_params)
     render json: bird, status: :created
   end
-
+  
   # GET /birds/:id
   def show
     bird = Bird.find_by(id: params[:id])
@@ -20,6 +20,12 @@ class BirdsController < ApplicationController
     else
       render json: { error: "Bird not found" }, status: :not_found
     end
+  end
+
+  private
+
+  def bird_params
+    params.permit(:name, :species)
   end
 
 end
